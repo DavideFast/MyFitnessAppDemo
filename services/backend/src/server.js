d5e312db3d43e78a4fb15e2db29b05bf16c4ab7d
DavideFast <davidefast99@gmail.com>
2026-09-09 15:02:28 +0200

    Update server.js

--- services/backend/src/server.js
+++ services/backend/src/server.js
@@ -352,7 +352,7 @@ app.use(
 app.use(express.json());
 app.use(createSystemRouter());
 
-// =========================== READ/WRITE DATABASE  ==============================
+// =========================== ROUTE  ==============================
 app.get("/api/v1/readPostgresql", (req, res) => {
   const query = "SELECT * FROM allenamenti WHERE athlete_id = 1 LIMIT 10";
   context.pool.query(query, (err, result) => {
